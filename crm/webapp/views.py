88fe2d0917a61d789624b2f8636ab5508df40e9e
unknown <brannongarrett77@gmail.com>
2024-01-11 20:31:29 +0100

    Created base.html, templates and static folders, and all need files and code

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    #return HttpResponse("Hello, Django!")
     return render(request, "webapp/index.html")
