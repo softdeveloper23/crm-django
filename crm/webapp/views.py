2536a9383b52987b01949dcf1820cd17fa4d8524
unknown <brannongarrett77@gmail.com>
2024-01-12 19:36:03 +0100

    Added a registration form and links for registering

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
-
+# - Homepage
 def home(request):
     return render(request, "webapp/index.html")
+
+# - Register a new user
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #return redirect("")
+
+    context = {"form": form}
+    return render(request, "webapp/register.html", context=context)
