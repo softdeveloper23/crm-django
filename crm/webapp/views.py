28425675603ed18c15c72ca85e0e7def911c98db
unknown <brannongarrett77@gmail.com>
2024-01-12 20:25:35 +0100

    Created and coded login functionality

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
 
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
 # - Homepage
 def home(request):
     return render(request, "webapp/index.html")
@@ -13,7 +16,25 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            #return redirect("")
+            return redirect("login")
 
     context = {"form": form}
     return render(request, "webapp/register.html", context=context)
+
+# - Login a user
+def login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                #return redirect("")
+
+    context = {"form": form}
+    return render(request, "webapp/login.html", context=context)
