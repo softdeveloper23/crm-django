10352500a88c302bea1400353ece554ceb2357be
unknown <brannongarrett77@gmail.com>
2024-01-14 17:34:00 +0100

    Finished CRUD functionality

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -86,6 +86,14 @@ def singular_record(request, pk):
     context = {"record": all_records}
     return render(request, "webapp/view-record.html", context=context)
 
+# - Delete a record
+@login_required(login_url="login")
+def delete_record(request, pk):
+    record = Record.objects.get(id=pk)
+    record.delete()
+
+    return redirect("dashboard")
+
 # - Logout a user
 def logout(request):
     auth.logout(request)
