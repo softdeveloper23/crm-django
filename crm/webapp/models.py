c85a111b2dc6a36b5bd0880bebaa5e9d84d89241
unknown <brannongarrett77@gmail.com>
2024-01-14 14:52:49 +0100

    Added a record model in models.py

--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + "   " + self.last_name
